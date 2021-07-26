442255ad96f4a3a0bf70cea32fde7bd5aab3f681
vivek <you@example.com>
2021-07-26 23:43:28 +0530

    new code

--- Untitled-1.sh
+++ Untitled-1.sh
@@ -1,13 +1,20 @@
+
 #!/bin/bash
 
-sudo apt-get update
+sudo apt-get update -y
 sudo apt-get upgrade -y
-sudo apt-get install tasksel -y
-sudo tasksel install lamp-server
+sudo apt-get install dialog 
+DEBIAN_FRONTEND=noninteractive sudo apt-get install tasksel -y
+DEBIAN_FRONTEND=noninteractive sudo tasksel install lamp-server
 
-sudo mysql -u root -padmin -e "CREATE DATABASE drupal CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;"
-sudo mysql -u root -padmin -e "GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, DROP, INDEX, ALTER, CREATE TEMPORARY TABLES ON drupal.* TO ‘drupaluser’@’localhost’ IDENTIFIED BY 'root';"
-D9=drupal-9.2.2
+if [ -f /etc/init.d/mysql* ]
+then
+        echo "Already installed"
+else
+        sudo mysql -u root -padmin -e "CREATE DATABASE drupal CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;"
+        sudo mysql -u root -padmin -e "GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, DROP, INDEX, ALTER, CREATE TEMPORARY TABLES ON drupal.* TO ‘drupaluser’@’localhost’ IDENTIFIED BY 'root';"
+fi
+d9="drupal-9.2.2"
 wget https://ftp.drupal.org/files/projects/$D9.tar.gz
 tar -zxf $D9.tar.gz
 sudo mkdir /var/www/html/drupal
